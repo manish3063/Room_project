45d6f0435f7575fb197f9d8a9d806239a22dd48e
unknown <manishsingh111095@gmail.com>
2022-09-28 09:20:21 +0530

    Registration is added

---
+++ app/Http/Controllers/Reservation.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Reservations;
+use Validator;
+
+
+class Reservation extends Controller
+{
+    function Reservation(Request $req){
+
+        $user= new Reservations;
+
+        //if needed you can apply it
+
+        // if($user->arrival=$req->arrival==$user->departure=$req->departure){
+        //     return ["status"=>false,"message"=>"Arival Date and Departure date not be same"];
+
+        // }
+        
+        // check the same entry is entired at two times 
+        // $sameEntry =Reservations::where('guest_name',$req->guest_name);
+        // if($sameEntry){
+        //     return ["status"=>false,"message"=>"You have Already Registered with this userId"];
+        // }
+
+
+        $check=array(
+            "arrival"=>"required",
+            "guest_name"=>"required",
+            "departure"=>"required",
+            "phone_no"=>"required | max:13",
+            "city"=>"required",
+            "vou_no"=>"required",
+            "bkg_no"=>"required",
+            "status"=>"required"
+            
+);
+        $validator= Validator::make($req->all(),$check);
+
+        if($validator->fails())
+        {
+            return $validator->errors();
+
+        }else{
+       $user->arrival=$req->arrival;
+       $user->guest_name=$req->guest_name;
+       $user->departure=$req->departure;
+       $user->phone_no=$req->phone_no;
+       $user->city=$req->city;
+       $user->advance=$req->advance;
+       $user->vou_no=$req->vou_no;
+       $user->bkg_no=$req->bkg_no;
+       $user->status=$req->status;
+       $user->source_agent=$req->source_agent;
+        
+        
+        $result=$user->save();
+        
+        if($result){
+            return ["status"=>true,"message"=>"Data has been saved"];
+        }else{
+            return ["status"=>false,"message"=>"Data has not been saved"];
+        }
+
+
+        }
+
+       }
+    // This is the search function 
+       
+       function filter($guest_name){
+        
+        $userlist=Reservations::where("guest_name",$guest_name)->get();
+        return $userlist;
+        
+        
+       }
+
+    //test
+    //     function filter(Request $req){
+    //     $user =new Reservations;
+
+    //     $user->guest_name=$req->guest_name;
+    //     $result=Reservations::where("guest_name",$user)->get();
+
+    //     return $result;
+       
+       
+        
+        
+    //    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserRegister;
 use App\Http\Controllers\Login;
+use App\Http\Controllers\Reservation;
 
 
 /*
@@ -23,3 +24,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::post("register",[UserRegister::class,'Register']);
 Route::post("login",[Login::class,'Login']);
+Route::post("reservation",[Reservation::class,'Reservation']);
+Route::get("find/{guest_name}",[Reservation::class,'filter']);
+//Route::post("find",[Reservation::class,'filter']);
